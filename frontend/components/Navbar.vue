fd4b1c5ad1404b3855d6ac863c2e2843cdd3b501
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-29 22:40:52 +0100

    added navbar

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar center-collapsed v-model="active">
+      <template #left>
+        <h3>SlimLink</h3>
+      </template>
+      <vs-navbar-item :active="active == 'home'" id="home">
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'about'" id="about">
+        About
+      </vs-navbar-item>
+      <template #right>
+        <vs-button flat>Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    active: 'home',
+  }),
+})
+</script>
